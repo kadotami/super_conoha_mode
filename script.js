76a71a305ac57812367455e30358e3990ff404b7
yuta-kadotami <yuta-kadotami@gmo.jp>
2017-06-16 12:22:22 +0900

    modify style

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
   if (domain === "manage.conoha.jp") {
     var ele = document.createElement("style");
     var str = document.createTextNode('.wrap {background: none}\
+                                      .title-area, .refine, .edit, .chosen-container,.btn-pager {background: rgba(255,255,255,0.7);}\
                                       ');
     ele.appendChild(str);
     document.body.appendChild(ele);
